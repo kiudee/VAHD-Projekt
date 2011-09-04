1dbd75016a55df0c8d8349a7345e3b0fdee48e40
Karlson Pfannschmidt <kiudee@NoMacSlave.localdomain>
2011-09-04 18:46:50 +0200

    Changed types to standard-conform lib.

--- Hashtable.cpp
+++ Hashtable.cpp
@@ -1,23 +1,21 @@
-#include <iostream>
 #include <math.h>
+#include <iostream>
 #include "Subjects1-6.h"
 #include "Objects.h"
 #include "Node.h"
 #include "Supervisor.h"
 
-typedef unsigned long uint32;
-typedef unsigned long long uint64;
-static const uint32 s = 2654435769;
+static const uint32_t s = 2654435769;
 
 
 double mult_hash(int kk)
 {
-    uint32 i = 15;// m = 2^i
-    uint64 k = (uint32) kk;
-    uint64 t = s * k & 0x00000000FFFFFFFF;
+    uint32_t i = 15;  // m = 2^i
+    uint64_t k = static_cast<uint32_t>(kk);
+    uint64_t t = s * k & 0x00000000FFFFFFFF;
     t = t >> (32 - i);
 
-    return t/(double)(2 << 14);
+    return t/static_cast<double>(2 << 14);
 }
 
 int main()
